24b754618e43ff80d5c61c5c44532935a1d2a189
taka14aka-hub <taka14.aka@gmail.com>
2026-08-15 18:27:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if prompt := st.chat_input("Например: Проведи аудит разд
         # Вызов модели
         with st.chat_message("assistant"):
             model = genai.GenerativeModel(
-                model_name="gemini-1.5-flash-latest",
+                model_name="gemini-1.0-pro",
                 system_instruction=system_instruction
             )
             
